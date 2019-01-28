1bbb2c7b3486ca4dd589777b115b4b4535646d51
jessix00 <jesscasillas24@gmail.com>
2019-01-28 16:25:41 -0700

    Creating multiple cat counters.ToDo-Dynamically add cat images/counter.

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,7 +1,34 @@
-const countingCats = document.getElementById("kitten-to-click");
-const display = document.getElementById("counter");
-let countOne = 0;
+function clickTracker() {
+    catPictureOne = document.getElementById('catPictureOne').onclick = function() { clickerOne() };
+    catPictureTwo = document.getElementById('catPictureTwo').onclick = function() { clickerTwo() };
+    counterOne = document.getElementById('counterOne');
+    counterTwo = document.getElementById('counterTwo');
+    let clickCounter1 = 0;
+    let clickCounter2 = 0;
 
-countingCats.onclick = function() {
-    display.innerHTML = countOne += 1;
+
+    function clickerOne() {
+        counterOne.innerHTML = ` ${clickCounter1 += 1}`;
+    }
+
+    function clickerTwo() {
+        counterTwo.innerHTML = ` ${clickCounter2 += 1}`;
+    }
+
 }
+
+clickTracker();
+
+//Programatically adding Cat Names to our DOM
+function addTheNames() {
+    //array of our cat names
+    catNames = ["Kitty", "Blue"];
+    //getElementsByClassName returns a HTML Collection 
+    catNamesHTML = document.getElementsByClassName('catNames');
+    //We have to use .item to specify what element to select since 
+    //HTML Collection is not an array but a list of nodes
+    catNameOnes = catNamesHTML.item(0).innerHTML = catNames[0];
+    catNameTwo = catNamesHTML.item(1).innerHTML = catNames[1];
+}
+//calls function
+addTheNames();
